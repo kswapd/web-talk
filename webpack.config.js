0d19d1f6c6471cce3dea22f1e98df4f5b759b5f1
kongxiangwen <xw.kong@hnair.com>
2016-10-30 23:07:22 +0800

    Packet coffee file to bundle javascript file by webpack automatically.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-     entry: './public/main.js',
+     entry: './public/main.coffee',
      output: {
          path: './public',
          filename: 'app.bundle.js'
-     }
+     },
+     module: {
+		loaders: [
+			{ test: /\.coffee$/, loader: "coffee", exclude:/node_modules/ }
+		]
+	}
  };
