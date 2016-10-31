c570ebc4047c2ab8e8b9264d33cd3663ac9cc9b7
kongxiangwen <xw.kong@hnair.com>
2016-10-31 21:40:54 +0800

    add script file to restart talk process.

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
-npm run init
-npm run start
+#cd /src/talk-account && gulp build
+#cd /src/talk-web && gulp build 
+
+log="talk.log"
+token="web-talk"
+rm $log
+pid=`ps -ef|grep $token | grep -v "grep"|awk '{print $2}'`
+
+if [ "$pid" = "" ]
+then
+  echo "No $token process, start it."
+else
+ kill -9 $pid
+ echo "Has $token process at pid:$pid, restart it."
+fi
+ npm run init >$log 2>&1
+ nohup npm start >$log 2>&1&
+ pid2=`ps -ef|grep $token | grep -v "grep"|awk '{print $2}'`
+ echo "Start $token process at pid $pid2 success."
+
